4141912d276822054cf61048df277cb316e31dbd
bymbaamedehgui-beep <bymbaamedehgui@gmail.com>
2026-08-13 17:10:17 +0800

    feat: V бүлэг — Жишиг 1,2 ба Шалгалтын материал (5.1-5.6 хамарсан; тест 10 + задгай 4, олон хувилбар)

--- api/seed.js
+++ api/seed.js
@@ -119,6 +119,7 @@ module.exports = async (req, res) => {
         { grade: '10-р анги', name: '5.4 y = axⁿ хэлбэрийн функцийн график', slugs: ['funkts-axn-muj-10.html', 'funkts-ax2inv-graf-10.html', 'funkts-sqrt-cube-graf-10.html'] },
         { grade: '10-р анги', name: '5.5 y = aˣ илтгэгч функцийн график', slugs: ['funkts-exp-graf-10.html', 'funkts-exp-tegsh-10.html'] },
         { grade: '10-р анги', name: '5.6 Муруйн шүргэгч, шүргэгчийн налалт', slugs: ['shurgegch-nalalt-1-10.html', 'shurgegch-nalalt-2-10.html'] },
+        { grade: '10-р анги', name: 'V бүлэг — Жишиг ба шалгалт', slugs: ['jishig-5-1-10.html', 'jishig-5-2-10.html', 'shalgalt-material-5-10.html'] },
         { grade: '10-р анги', name: 'Квадрат график — графикаас тэгшитгэл', slugs: ['grafik-kvadrat-tegsh.html'] },
       ];
       const seedAddRow = await pool.query(`SELECT sval FROM ws_settings WHERE skey='sg_seeded_add'`);
