a9a1f47fb641e12df8bc176a135d1afe0e671202
bymbaamedehgui-beep <bymbaamedehgui@gmail.com>
2026-08-13 14:56:31 +0800

    feat: Графикаас квадрат функцийн тэгшитгэл бичих ажлын хуудас (парабол + орой ба тусламжийн цэг, a=y-k)

--- catalog.js
+++ catalog.js
@@ -223,6 +223,7 @@ window.WS_LIST=[
     ['funkts-mon-eseh-2-10.html','Харгалзаа функц мөн эсэх 2'],
     ['funkts-ax-graf-10.html','y = a/x функцийн график (урвуу пропорциональ)'],
     ['funkts-ax-tegsh-10.html','Графикаас y = a/x тэгшитгэл бичих'],
+    ['grafik-kvadrat-tegsh.html','Графикаас квадрат функцийн тэгшитгэл бичих'],
     ['grafik-ax2.html','Квадрат график: y = ax² (орой эх цэгт)'],
     ['grafik-ax2-c.html','Квадрат график: y = ax² + c (босоо зөөлт)'],
     ['grafik-ax-h2.html','Квадрат график: y = a(x−h)² (хэвтээ зөөлт)'],
